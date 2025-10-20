ba27922ba1b08e4156689be3e5ebba4d8b2660fa
nams <88126281+namitabinu@users.noreply.github.com>
2025-10-20 18:38:55 +0200

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,35 +1,26 @@
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Question implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private int id;
     private String question;
     private String correctAnswer;
-    private List<String> wrongAnswers;
-    private int difficulty; // 1 = Easy, 2 = Medium, 3 = Hard
+    private ArrayList<String> wrongAnswers;
 
-    public Question(int id, String question, String correctAnswer,
-            List<String> wrongAnswers, int difficulty) {
-        this.id = id;
+    public Question(String question, String correctAnswer, ArrayList wrongAnswers) {
         this.question = question;
         this.correctAnswer = correctAnswer;
         this.wrongAnswers = wrongAnswers;
-        this.difficulty = difficulty;
     }
 
-    public List<String> getAllAnswersShuffled() {
-        List<String> allAnswers = new ArrayList<>();
+    public ArrayList getAllAnswersShuffled() {
+        ArrayList<String> allAnswers = new ArrayList<String>();
         allAnswers.add(correctAnswer);
         allAnswers.addAll(wrongAnswers);
         Collections.shuffle(allAnswers);
         return allAnswers;
-    }
- 
-    // Getters
-    public int getId() {
-        return id;
     }
 
     public String getQuestion() {
@@ -40,11 +31,7 @@ public class Question implements Serializable {
         return correctAnswer;
     }
 
-    public List<String> getWrongAnswers() {
+    public ArrayList<String> getWrongAnswers() {
         return wrongAnswers;
-    }
-
-    public int getDifficulty() {
-        return difficulty;
     }
 }
